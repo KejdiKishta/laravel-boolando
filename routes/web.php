2523ba4b7869b3d8434baae045ce78744d9c3e53
Kejdi Kishta <155896977+KejdiKishta@users.noreply.github.com>
2024-06-07 19:00:01 +0200

    add: header logo and icons

--- routes/web.php
+++ routes/web.php
@@ -15,9 +15,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     $navList = ["Donna", "Uomo", "Bambini"];
+    $headerIcons = ["fa-regular fa-user", "fa-regular fa-heart", "fa-solid fa-bag-shopping"];
 
     $data = [
         "navList" => $navList,
+        "headerIcons" => $headerIcons,
     ];
     return view('welcome', $data);
 });
