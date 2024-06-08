ca08f8f55c2050b6e8a1bba492ef49b49b98937e
Kejdi Kishta <155896977+KejdiKishta@users.noreply.github.com>
2024-06-08 12:43:45 +0200

    main refactoring

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,32 +13,6 @@
     {{-- header --}}
     @include('partials.header')
     {{-- main --}}
-    <main>
-        <div class="container py-5">
-            <div class="row">
-                {{-- products --}}
-                @foreach ($products as $product)
-                <div class="col-4 p-3 position-relative">
-                        {{-- images --}}
-                        <img src="{{ Vite::asset("resources/img/" . $product["frontImage"]) }}" alt="">
-                        {{-- info --}}
-                        <div class="text-secondary ms_font-size">{{ $product["brand"] }}</div>
-                        <h6 class="text-uppercase fw-bold m-0">{{ $product["name"] }}</h6>
-                        <span class="ms_font-size ms_red fw-bold">{{ $product["price"] }}</span>
-                        {{-- favorites btn --}}
-                        <button type="button" class="btn rounded-0 py-2 px-3 position-absolute bg-white ms_heart-position">
-                            <i class="fa-solid fa-heart {{$product["isInFavorites"] ? 'ms_red' : ''}} "></i>
-                        </button>
-                        {{-- badges --}}
-                        <div class="position-absolute ms_badges-position">
-                            @foreach ($product["badges"] as $badge)
-                                <span class="px-2 py-1 text-white fw-bold ms_font-size {{ $badge["type"] === "tag" ? 'ms_bg-green' : 'ms_bg-red'}}">{{ $badge["value"] }}</span>
-                            @endforeach
-                        </div>
-                    </div>
-                @endforeach
-            </div>
-        </div>
-    </main>
+    @include('partials.main')
 </body>
 </html>
