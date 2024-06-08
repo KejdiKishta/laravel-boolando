930c4343f207fe4860e098a02c6bbeb6b48e2f3b
Kejdi Kishta <155896977+KejdiKishta@users.noreply.github.com>
2024-06-08 16:07:10 +0200

    footer refactoring

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -15,39 +15,6 @@
     {{-- main --}}
     @include('partials.main')
     {{-- footer --}}
-    <footer class="bg-dark">
-        <div class="container py-4">
-            <div class="row">
-                {{-- name and info --}}
-                <div class="col text-white">
-                    {{-- name --}}
-                    <h6 class="fw-bold">Boolando s.r.l.</h6>
-                    {{-- info --}}
-                    <ul class="d-flex p-0 gap-2 ms_font-size">
-                        <li>
-                            <a href="#">Informazioni legali</a>
-                        </li>
-                        <li>
-                            <a href="#">Informazioni sulla privacy</a>
-                        </li>
-                        <li>
-                            <a href="#">Diritti di recesso</a>
-                        </li>
-                    </ul>
-                </div>
-                {{-- socials --}}
-                <div class="col text-white text-end">
-                    <div class="fw-bold ms_font-size">Trovaci anche su</div>
-                    <ul class="d-flex justify-content-end p-0 gap-2">
-                        @foreach ($footerIcons as $icon)
-                            <li>
-                                <i class="{{ $icon }}"></i>
-                            </li>
-                        @endforeach
-                    </ul>
-                </div>
-            </div>
-        </div>
-    </footer>
+    @include('partials.footer')
 </body>
 </html>
