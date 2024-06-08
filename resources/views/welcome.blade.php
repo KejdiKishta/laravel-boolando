cae9cc115187df6785633920216406efcec9ef98
Kejdi Kishta <155896977+KejdiKishta@users.noreply.github.com>
2024-06-08 11:34:36 +0200

    add: product info

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -16,10 +16,15 @@
     <main>
         <div class="container py-5">
             <div class="row">
-                {{-- images --}}
+                {{-- products --}}
                 @foreach ($products as $product)
-                    <div class="col-4 p-2">
+                <div class="col-4 p-3">
+                        {{-- images --}}
                         <img src="{{ Vite::asset("resources/img/" . $product["frontImage"]) }}" alt="">
+                        {{-- info --}}
+                        <div class="text-secondary ms_font-size">{{ $product["brand"] }}</div>
+                        <h6 class="text-uppercase fw-bold m-0">{{ $product["name"] }}</h6>
+                        <span class="ms_font-size ms_red fw-bold">{{ $product["price"] }}</span>
                     </div>
                 @endforeach
             </div>
